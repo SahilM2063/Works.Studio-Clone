37f4447d479daf64e39fa962b5f74d08cb037c3a
Sahil Makadia <makadiasahil9846@gmail.com>
2024-01-29 16:41:26 +0530

    Final animation and footer added

--- script.js
+++ script.js
@@ -55,3 +55,53 @@ elems.forEach(function (elem) {
     })
 })
 
+
+// scroll back to top
+
+document.querySelector("#page3 a").addEventListener("click", function () {
+    scroll.scrollTo(0)
+})
+
+// page by page scroll
+
+let screenHeight = window.innerHeight;
+
+document.querySelector("#page1 .page-footer a").addEventListener("click", function () {
+    scroll.scrollTo(screenHeight)
+})
+
+document.querySelector("#page2 .page-footer a").addEventListener("click", function () {
+    scroll.scrollTo(screenHeight + screenHeight)
+})
+
+window.addEventListener("resize", function () {
+    screenHeight = window.innerHeight
+})
+
+
+// navbar opening and closing 
+const nav_elems = document.querySelectorAll("#nav .nav-right .elems");
+const navLink = document.querySelector("#nav .nav-right a");
+
+var isOpen = true; // Initially, the navigation is closed
+
+// Function to toggle navigation elements
+function toggleNav() {
+    var nl = gsap.timeline();
+    if (isOpen) {
+        navLink.style.transform = "rotate(-135deg)"
+        nl.to(nav_elems, { x: "0%", stagger: 0.1, ease: "expo.out", duration: 1 }, "yup");
+        nl.to(".hide", { opacity: 1, visibility: "visible", stagger: 0.3 }, "yup");
+    } else {
+        navLink.style.transform = "rotate(0deg)"
+        nl.to(nav_elems, { x: "78%", stagger: 0.1, ease: "expo.out", duration: 1 }, "yo");
+        nl.to(".hide", { opacity: 0, visibility: "hidden", }, "yo");
+    }
+    isOpen = !isOpen; // Toggle the isOpen state
+}
+
+// Event listener for navigation click
+toggleNav()
+navLink.addEventListener("click", function () {
+    toggleNav(); // Play the timeline when clicked
+});
